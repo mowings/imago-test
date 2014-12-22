5e9572fccce67d6eac46724b98fcbebde0562f45
Michael Owings <mowings@turbosquid.com>
2014-12-22 13:24:14 -0600

    Stats, less verbose

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"sync"
+	"time"
 )
 
 const SERVER = "http://localhost:3000"
@@ -38,15 +39,27 @@ func scale(worker_id int) bool {
 		Actions: []Action{
 			{
 				"s3://turbosquid-hackathon/imago/foo.png",
-				fmt.Sprintf("s3://turbosquid-hackathon/imago/out/foo-%d.jpg", worker_id),
+				fmt.Sprintf("s3://turbosquid-hackathon/imago/out/foo200x200-%d.jpg", worker_id),
 				"image/jpeg",
-				[]string{"resize 200x200", "quality 100"},
+				[]string{"resize 200x200", "quality 50"},
 			},
 			{
 				"s3://turbosquid-hackathon/imago/foo.png",
-				fmt.Sprintf("s3://turbosquid-hackathon/imago/out/foo-%d.tiff", worker_id),
-				"image/tiff",
-				[]string{"resize 2000x2000"},
+				fmt.Sprintf("s3://turbosquid-hackathon/imago/out/foo400x400-%d.jpg", worker_id),
+				"image/jpg",
+				[]string{"resize 400x400"},
+			},
+			{
+				"s3://turbosquid-hackathon/imago/foo.png",
+				fmt.Sprintf("s3://turbosquid-hackathon/imago/out/foo600x600-%d.jpg", worker_id),
+				"image/jpg",
+				[]string{"resize 600x600 -quality 100"},
+			},
+			{
+				"s3://turbosquid-hackathon/imago/foo.png",
+				fmt.Sprintf("s3://turbosquid-hackathon/imago/out/foo1200x1200-%d.jpg", worker_id),
+				"image/jpg",
+				[]string{"resize 1200x1200 -quality 100"},
 			},
 		},
 	}
@@ -63,7 +76,6 @@ func scale(worker_id int) bool {
 	defer resp.Body.Close()
 	dec := json.NewDecoder(resp.Body)
 	_ = dec.Decode(&server_response)
-	log.Printf("Id: %s, queue_length: %d\n", server_response["id"], int(server_response["queue_length"].(float64)))
 	resp, err = http.Get(SERVER + "/api/v1/work/" + server_response["id"].(string) + "?timeout=300")
 	if err != nil {
 		return false
@@ -72,13 +84,9 @@ func scale(worker_id int) bool {
 		dec = json.NewDecoder(resp.Body)
 		_ = dec.Decode(&server_response)
 		if server_response["status"] == "error" {
-			log.Println("conversion failed.")
 			return false
-		} else {
-			log.Println("conversion complete")
 		}
 	} else {
-		log.Printf("Conversion failed. Status: %d\n", resp.StatusCode)
 		return false
 	}
 	return true
@@ -97,18 +105,36 @@ func worker(id int, count int, done chan int) {
 	done <- id
 }
 
+func log_stats(c chan bool) {
+	for {
+		select {
+		case <-c:
+			return
+		case <-time.After(time.Second * 10):
+			mutex.Lock()
+			out_total := total
+			out_errors := errors
+			mutex.Unlock()
+			log.Printf("Total: %d, errors: %d\n", out_total, out_errors)
+		}
+	}
+}
+
 func main() {
 	concurrency := flag.Int("concurrency", 1, "Max concurrent requests")
 	count := flag.Int("count", 5, "Total requests (per worker)")
 	flag.Parse()
 	log.Printf("Running test with %d workers, %d requests per worker.\n", *concurrency, *count)
 	c := make(chan int, *concurrency)
+	d := make(chan bool)
 	for i := 0; i < *concurrency; i++ {
 		go worker(i, *count, c)
 	}
+	go log_stats(d)
 	for i := 0; i < *concurrency; i++ {
 		id := <-c
 		log.Printf("Worker %d finished\n", id)
 	}
 	log.Printf("Total: %d, errors: %d\n", total, errors)
+
 }
