830360c912986156ee25b5323417487016cc4a58
Michael Owings <mowings@turbosquid.com>
2014-12-21 21:46:27 -0600

    frist pu5h

---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+const SERVER = "http://localhost:3000"
+
+type Action struct {
+	Infile     string   `json:"infile"`
+	Outfile    string   `json:"outfile"`
+	Mimetype   string   `json:"mimetype"`
+	Operations []string `json:"operations"`
+}
+
+type Request struct {
+	Actions []Action `json:"actions"`
+}
+
+var total int
+var errors int
+var mutex = &sync.Mutex{}
+
+func scale(worker_id int) bool {
+	var err error
+	defer func() {
+		if err != nil {
+			log.Println(err)
+		}
+	}()
+	req := Request{
+		Actions: []Action{
+			{
+				"s3://turbosquid-hackathon/imago/foo.png",
+				fmt.Sprintf("s3://turbosquid-hackathon/imago/out/foo-%d.jpg", worker_id),
+				"image/jpeg",
+				[]string{"resize 200x200", "quality 100"},
+			},
+			{
+				"s3://turbosquid-hackathon/imago/foo.png",
+				fmt.Sprintf("s3://turbosquid-hackathon/imago/out/foo-%d.tiff", worker_id),
+				"image/tiff",
+				[]string{"resize 4000x4000"},
+			},
+		},
+	}
+	out, _ := json.Marshal(req)
+	resp, err := http.Post(SERVER+"/api/v1/work", "application/json", bytes.NewBuffer(out))
+	if err != nil {
+		return false
+	}
+	if resp.StatusCode != 200 {
+		log.Println("status: ", resp.Status)
+		return false
+	}
+	var server_response map[string]interface{}
+	dec := json.NewDecoder(resp.Body)
+	_ = dec.Decode(&server_response)
+	log.Printf("Id: %s, queue_length: %f\n", server_response["id"], server_response["queue_length"])
+	resp, err = http.Get(SERVER + "/api/v1/work/" + server_response["id"].(string) + "?timeout=300")
+	if err != nil {
+		return false
+	}
+	if resp.StatusCode == 200 {
+		log.Println("Conversion complete")
+	} else {
+		log.Printf("Conversion failed. Status: %d\n", resp.StatusCode)
+		return false
+	}
+	return true
+}
+
+func worker(id int, count int, done chan int) {
+	for i := 0; i < count; i++ {
+		ok := scale(id)
+		mutex.Lock()
+		total++
+		if !ok {
+			errors++
+		}
+		mutex.Unlock()
+	}
+	done <- id
+}
+
+func main() {
+	concurrency := flag.Int("concurrency", 1, "Max concurrent requests")
+	count := flag.Int("count", 5, "Total requests (per worker)")
+	flag.Parse()
+	log.Printf("Running test with %d workers, %d requests per worker.\n", *concurrency, *count)
+	c := make(chan int, *concurrency)
+	for i := 0; i < *concurrency; i++ {
+		go worker(i, *count, c)
+	}
+	for i := 0; i < *concurrency; i++ {
+		id := <-c
+		log.Printf("Worker %d finished\n", id)
+	}
+	log.Printf("Total: %d, errors: %d\n", total, errors)
+}
